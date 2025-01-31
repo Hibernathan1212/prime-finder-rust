fe09ff8c65c741b1c4ba2e5c8482f5c38215a215
Nathan <nnthurber@icloud.com>
2025-01-31 17:32:17 +0700

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use std::fs::File;
 use std::io::Write;
 use std::io;
 
-fn prime_finder(limit: usize) {
+fn prime_finder(limit: usize, file_path: &str) {
 
     let mut prime = vec![true; limit / 2];
 
-    let mut file = File::create("/Users/nathan/Downloads/Primes.txt").expect("Unable to create file");
+    let mut file = File::create(file_path).expect("Unable to create file");
 
     writeln!(file, "2").unwrap();
     writeln!(file, "3").unwrap();
@@ -35,15 +35,19 @@ fn prime_finder(limit: usize) {
 }
 
 fn main() {
-
-    // Call the prime_finder function
     println!("Enter the limit for prime finding:");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
     let limit: usize = input.trim().parse().expect("Please enter a valid number");
 
+    println!("Enter file path to save the prime numbers:");
+    let mut file_path = String::new();
+    io::stdin().read_line(&mut file_path).expect("Failed to read line");
+    let file_path = file_path.trim();
+
     let start = std::time::Instant::now();
-    prime_finder(limit);
+
+    prime_finder(limit, file_path);
     let elapsed = start.elapsed();
     println!("Total elapsed time: {:.3}s", elapsed.as_secs_f64());
 }
